821795912b87fb12cf7df194ac068d26df5a2134
AlAzmine482 <137209260+AlAzmine482@users.noreply.github.com>
2026-07-28 11:33:08 -0700

    Add: car-pooling - submission-2

---
+++ Data Structures & Algorithms/car-pooling/submission-2.py
@@ -0,0 +1,23 @@
+import heapq
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        if not trips:
+            return 0
+        trips.sort(key=lambda x: x[1])
+        min_heap = []
+        curr_pass = 0
+        for num_passengers, start_loc, end_loc in trips:
+            # 1. DROP OFF: Remove all passengers whose destination is <= current start_loc
+            while min_heap and min_heap[0][0] <= start_loc:
+                dropped_end, dropped_pass = heapq.heappop(min_heap)
+                curr_pass -= dropped_pass
+
+            # 2. PICK UP: Add the new passengers to the car
+            curr_pass += num_passengers
+            heapq.heappush(min_heap, (end_loc, num_passengers))
+
+            # 3. CAPACITY CHECK: Did we overload the car?
+            if curr_pass > capacity:
+                return False
+        return True
+         
